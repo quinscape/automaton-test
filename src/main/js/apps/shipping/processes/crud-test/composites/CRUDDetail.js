6f2c9faa90221808a4e3b3d5273f1c87a7ed0064
Sven Helmberger <sven.helmberger@quinscape.de>
2019-03-04 17:26:26 +0100

    Encapuslate live form options.
    Move some of the state handling out of CRUDDetail and into its own composite.
    
    We need the control object to pass it down to the <FooForm/>

--- src/main/js/apps/shipping/processes/crud-test/composites/CRUDDetail.js
+++ src/main/js/apps/shipping/processes/crud-test/composites/CRUDDetail.js
@@ -1,23 +1,18 @@
 import React, { useState } from "react"
 import { observer as fnObserver } from "mobx-react-lite";
-
-import { Icon } from "@quinscape/automaton-js"
-import { DEFAULT_OPTIONS, UserFormControl } from "domainql-form"
+import { DEFAULT_OPTIONS } from "domainql-form"
 import FooForm from "./FooForm";
-import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import FormOptions from "./FormOptions";
 
 
 const CRUDDetail = props => {
 
     const [ control, setControl] = useState(DEFAULT_OPTIONS);
-    const [ modalOpen, setModalOpen] = useState(false);
 
     const changeControl = (k, v) => setControl({
         ...control,
         [k]: v
     });
-
-    const close = () => setModalOpen(false);
 
     const {env} = props;
     const {scope} = env;
@@ -25,13 +20,10 @@ const CRUDDetail = props => {
     return (
         <React.Fragment>
 
-            <div className="row">
-                <div className="col">
-                    <button className="btn btn-light float-right" title="Form Settings" onClick={ () => setModalOpen(true) }>
-                        <Icon className="fa-cog"/>
-                    </button>
-                </div>
-            </div>
+             <FormOptions
+                control={ control }
+                changeControl={ changeControl }
+             />
 
             <div className="row">
 
@@ -40,22 +32,6 @@ const CRUDDetail = props => {
                 </div>
             </div>
 
-            <Modal isOpen={ modalOpen } toggle={ close } >
-                <ModalHeader toggle={ close }>
-                    User Form Control
-                </ModalHeader>
-                <ModalBody>
-                    <UserFormControl
-                        control={control}
-                        changeControl={changeControl}
-                    />
-                </ModalBody>
-                <ModalFooter>
-                    <button className="btn btn-primary" onClick={ () => setModalOpen(false) }>
-                        Close
-                    </button>
-                </ModalFooter>
-            </Modal>
 
         </React.Fragment>
     )
