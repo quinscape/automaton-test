9d8635b450452fa0c5e22d92acccb35d24c0bab4
Sven Helmberger <sven.helmberger@quinscape.de>
2019-09-17 13:10:44 +0200

    Standard op implementations
    Add beans implementing IdGenerator, StoreOperation and
    BatchStoreOperation.

--- src/main/java/de/quinscape/automatontest/runtime/config/ServiceConfiguration.java
+++ src/main/java/de/quinscape/automatontest/runtime/config/ServiceConfiguration.java
@@ -1,16 +1,26 @@
 package de.quinscape.automatontest.runtime.config;
 
+import de.quinscape.automaton.runtime.domain.IdGenerator;
+import de.quinscape.automaton.runtime.domain.SequenceIdGenerator;
+import de.quinscape.automaton.runtime.domain.UUIDGenerator;
+import de.quinscape.automaton.runtime.domain.op.BatchStoreOperation;
+import de.quinscape.automaton.runtime.domain.op.DefaultBatchStoreOperation;
+import de.quinscape.automaton.runtime.domain.op.DefaultStoreOperation;
+import de.quinscape.automaton.runtime.domain.op.StoreOperation;
 import de.quinscape.automatontest.model.ValidationRules;
+import de.quinscape.domainql.DomainQL;
 import de.quinscape.spring.jsview.loader.FileResourceLoader;
 import de.quinscape.spring.jsview.loader.JSONResourceConverter;
 import de.quinscape.spring.jsview.loader.ResourceHandle;
 import de.quinscape.spring.jsview.loader.ResourceLoader;
+import org.jooq.DSLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.util.StringUtils;
 
 import java.io.File;
@@ -50,5 +60,35 @@ public class ServiceConfiguration
             );
         }
         return handle;
+    }
+
+    @Bean
+    public IdGenerator uuidGenerator()
+    {
+        return new UUIDGenerator();
+    }
+
+    @Bean
+    public StoreOperation defaultStoreOperation(
+        DSLContext dslContext,
+        @Lazy DomainQL domainQL
+    )
+    {
+        return new DefaultStoreOperation(
+            dslContext,
+            domainQL
+        );
+    }
+
+    @Bean
+    public BatchStoreOperation defaultBatchStoreOperation(
+        DSLContext dslContext,
+        @Lazy DomainQL domainQL
+    )
+    {
+        return new DefaultBatchStoreOperation(
+            dslContext,
+            domainQL
+        );
     }
 }
