307e0858f2f6fbca33e051973c8e8537c7eb08c7
Sven Helmberger <sven.helmberger@quinscape.de>
2019-10-28 14:38:31 +0100

    Query-Test:
    Don't collapse JSON data, provide jump links/buttons to deal with data
    overload, provide one toolbar per query.
    
    Choose a dark theme to make navigation easier.
    
    While we're already hugely long as a view, let's also add the initial
    queries as <pre> blocks

--- src/main/js/apps/shipping/processes/query-test/composites/QueryTestHome.js
+++ src/main/js/apps/shipping/processes/query-test/composites/QueryTestHome.js
@@ -1,12 +1,7 @@
 import React from "react"
-import cx from "classnames"
 import { observer as fnObserver } from "mobx-react-lite";
-
-import { i18n, Button } from "@quinscape/automaton-js"
-import FooList from "../../../../../components/FooList";
-import Calendar from "react-calendar"
-import { ButtonToolbar, Row, Col, Card, CardHeader, CardBody } from "reactstrap";
-import ReactJson from 'react-json-view'
+import { ButtonToolbar, Col, Row } from "reactstrap";
+import ReactJson from "react-json-view"
 
 const QueryTestHome = props => {
 
@@ -20,36 +15,115 @@ const QueryTestHome = props => {
                 <h1>
                     Advanced nested queries.
                 </h1>
-                <ButtonToolbar>
-                    <button
-                        type="button"
-                        className="btn btn-secondary mr-1"
-                        onClick={ () => scope.bazFoos.update() }
-                    >
-                        Update 'bazFoos'
-                    </button>
-                    <button
-                        type="button"
-                        className="btn btn-secondary mr-1"
-                        onClick={ () => scope.bazesWithValues.update() }
-                    >
-                        Update 'bazesWithValues'
-                    </button>
-                    <button
-                        type="button"
-                        className="btn btn-secondary mr-1"
-                        onClick={ () => scope.nodes.update() }
-                    >
-                        Update 'nodes'
-                    </button>
-                </ButtonToolbar>
-                <ReactJson name="bazFoos" src={scope.bazFoos.rows} collapsed={ true }/>
-                <ReactJson name="bazesWithValues" src={scope.bazesWithValues.rows} collapsed={ true }/>
-                <ReactJson name="nodes" src={scope.nodes.rows} collapsed={ true }/>
+                <hr className="m-6"/>
+                <div id="bazFoos">
+                    <h2>
+                        bazFoos
+                    </h2>
+                    <ButtonToolbar>
+                        <button
+                            type="button"
+                            className="btn btn-secondary mr-1"
+                            onClick={ () => scope.bazFoos.update() }
+                        >
+                            Rerun 'bazFoos' query
+                        </button>
+                        <a
+                            className="btn btn-secondary mr-1"
+                            href="#bazesWithValues"
+                        >
+                            Scroll to bazesWithValues
+                        </a>
+                        <a
+                            className="btn btn-secondary mr-1"
+                            href="#nodes"
+                        >
+                            Scroll to nodes
+                        </a>
+                    </ButtonToolbar>
+                    <div className="mt-3">
+                                                <pre>
+                            {
+                                scope.bazFoos._query.query
+                            }
+                        </pre>
 
+                        <ReactJson name="bazFoos" theme="flat" src={scope.bazFoos.rows} />
+                    </div>
+                </div>
+                <hr className="m-6"/>
+                <div id="bazesWithValues">
+                    <h2>
+                        bazesWithValues
+                    </h2>
+                    <ButtonToolbar>
+                        <button
+                            type="button"
+                            className="btn btn-secondary mr-1"
+                            onClick={ () => scope.bazesWithValues.update() }
+                        >
+                            Rerun 'bazesWithValues' query
+                        </button>
+                        <a
+                            className="btn btn-secondary mr-1"
+                            href="#bazFoos"
+                        >
+                            Scroll to foo (top)
+                        </a>
+                        <a
+                            className="btn btn-secondary mr-1"
+                            href="#nodes"
+                        >
+                            Scroll to nodes
+                        </a>
+                    </ButtonToolbar>
+                    <div className="mt-3">
+                        <pre>
+                            {
+                                scope.bazesWithValues._query.query
+                            }
+                        </pre>
+                        <ReactJson name="bazesWithValues" theme="flat" src={scope.bazesWithValues.rows}/>
+                    </div>
+                </div>
 
-
+                <hr className="m-6"/>
+                <div id="nodes">
+                    <h2>
+                        nodes (recursive)
+                    </h2>
+                    <ButtonToolbar>
+                        <button
+                            type="button"
+                            className="btn btn-secondary mr-1"
+                            onClick={ () => scope.nodes.update() }
+                        >
+                            Rerun 'nodes' query
+                        </button>
+                        <a
+                            className="btn btn-secondary mr-1"
+                            href="#bazFoos"
+                        >
+                            Scroll to foo (top)
+                        </a>
+                        <a
+                            className="btn btn-secondary mr-1"
+                            href="#bazesWithValues"
+                        >
+                            Scroll to bazesWithValues
+                        </a>
+                    </ButtonToolbar>
+                    <div className="mt-3">
+                        <pre>
+                            {
+                                scope.nodes._query.query
+                            }
+                        </pre>
+                        <ReactJson name="nodes" theme="flat" src={scope.nodes.rows}/>
+                    </div>
+                </div>
             </Col>
+
         </Row>
     )
 };
