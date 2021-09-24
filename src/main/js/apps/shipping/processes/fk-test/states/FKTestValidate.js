7e3349cfa9ed854c677849577d9fdcf7b147a350
Sven Helmberger <sven.helmberger@quinscape.de>
2021-09-24 14:44:40 +0200

    fk-test fix

--- src/main/js/apps/shipping/processes/fk-test/states/FKTestValidate.js
+++ src/main/js/apps/shipping/processes/fk-test/states/FKTestValidate.js
@@ -1,13 +1,23 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
+import cx from "classnames";
 
-import { Button, config, createDomainObject, FKSelector, i18n, ViewState, } from "@quinscape/automaton-js";
+import { Button, config, createDomainObject, FKSelector, i18n, ViewState, getWireFormat } from "@quinscape/automaton-js";
 
 import FKTestList from "./FKTestList";
 import { Field, Form, FormContext, GlobalConfig, GlobalErrors, Icon } from "domainql-form";
 import Q_QuxA from "../../../queries/Q_QuxA";
 import { ButtonToolbar } from "reactstrap";
-import { useLocalObservable } from "mobx-react-lite";
+import { useLocalObservable, observer } from "mobx-react-lite";
 import { toJS } from "mobx";
+import Q_QuxC from "../../../queries/Q_QuxC";
+
+function createQuxMain(template)
+{
+    return getWireFormat().convert({
+        kind: "OBJECT",
+        name: "QuxMain"
+    }, template, true)
+}
 
 
 const FKTestValidate = new ViewState("FKTestValidate", (process, scope) => ({
@@ -43,7 +53,7 @@ const FKTestValidate = new ViewState("FKTestValidate", (process, scope) => ({
 
                         if (ctx.qualifiedName === "quxAId" && value === "3f88c30e-2bf9-458c-b016-dac001d02274")
                         {
-                            return "Negative";
+                            return "error from high-level validation";
                         }
                         return null
                     }
@@ -53,26 +63,115 @@ const FKTestValidate = new ViewState("FKTestValidate", (process, scope) => ({
         []
     )
 
-    const qux = useLocalObservable(() => {
-        const newObj = createDomainObject("QuxMain");
-        newObj.name = "Unnamed"
-        return newObj;
-    })
+    const [quxIndex,setQuxIndex] = useState(0);
 
+    const quxes = useLocalObservable(() => {
+        return [
+            // valid object
+            createQuxMain({
+                id: "365051d7-f10a-41fd-88ac-452a215595b1",
+                name: "Unnamed",
+                quxAId: "17861d28-a11d-4dd5-b66c-b4351ca1a980",
+                quxA: {
+                    name: "Qux A #3",
+                    value: 3
+                }
+
+            }),
+            // invalid selection according to local validation
+            createQuxMain({
+                id: "76cdfae9-7731-4c81-ad3b-fc5e1b8b293b",
+                name: "Unnamed #2",
+                quxAId: "cd816e04-a7cf-4df8-aad9-f021907cd81c",
+                quxA: {
+                    name: "Qux A #1",
+                    value: 1
+                }
+            }),
+            // invalid selection according to high-level validation
+            createQuxMain({
+                id: "bb658293-a4d3-412d-88b0-a804e5eea210",
+                name: "Unnamed #3",
+                quxAId: "3f88c30e-2bf9-458c-b016-dac001d02274",
+                quxA: {
+                    name: "Qux A #2",
+                    value: 2
+                }
+            }),
+            // invalid selection according to async validation
+            createQuxMain({
+                id: "64fbd80f-a4bf-41b6-8e41-cb62653cc9c8",
+                name: "Unnamed #4",
+                quxAId: "17861d28-a11d-4dd5-b66c-b4351ca1a980",
+                quxA: {
+                    name: "Qux A #3",
+                    value: 3
+                },
+                quxCId1: "1098633c-9260-4884-a952-03c122efe53b",
+                quxC1: {
+                    name: "Qux C #1",
+                    value: 1
+                }
+            })
+        ];
+    })
 
     //  console.log("CURRENT", toJS(scope.currentQux) )
 
+    const currentQux = quxes[quxIndex];
+
     return (
         <React.Fragment>
+            <div className="row">
+                <div className="col">
+                    <ButtonToolbar>
+                        <button
+                            type="button"
+                            className={ cx("btn btn-secondary mr-1", quxIndex === 0 && "active" ) }
+                            onClick={ () => setQuxIndex(0)}
+                            aria-pressed={ quxIndex === 0 }
+                        >
+                            Valid Object
+                        </button>
+                        <button
+                            type="button"
+                            className={ cx("btn btn-secondary mr-1", quxIndex === 1 && "active") }
+                            onClick={ () => setQuxIndex(1)}
+                            aria-pressed={ quxIndex === 1 }
+                        >
+                            Invalid Local
+                        </button>
+                        <button
+                            type="button"
+                            className={ cx("btn btn-secondary mr-1", quxIndex === 2 && "active") }
+                            onClick={ () => setQuxIndex(2)}
+                            aria-pressed={ quxIndex === 2 }
+                        >
+                            Invalid HL
+                        </button>
+                        <button
+                            type="button"
+                            className={ cx("btn btn-secondary mr-1", quxIndex === 3 && "active") }
+                            onClick={ () => setQuxIndex(3)}
+                            aria-pressed={ quxIndex === 3 }
+                        >
+                            Invalid Async
+                        </button>
+                    </ButtonToolbar>
+
+                    <hr/>
+                </div>
+            </div>
 
             <div className="row">
 
                 <div className="col">
                     <Form
+                        key={ currentQux.id }
                         formContext={ localFormContext }
                         type="QuxMainInput"
                         value={
-                            qux
+                            currentQux
                         }
                     >
                         {
@@ -94,16 +193,28 @@ const FKTestValidate = new ViewState("FKTestValidate", (process, scope) => ({
 
                                         <FKSelector
                                             name="quxAId"
-                                            label="simple search filter"
+                                            label="local and high-level validation"
                                             display="quxA.name"
                                             searchFilter="name"
                                             tooltip={ (root.quxA && root.quxA.name) || GlobalConfig.none() }
                                             query={ Q_QuxA }
                                             required={ true }
                                             validate={(ctx,value) => {
-                                                return value === "cd816e04-a7cf-4df8-aad9-f021907cd81c" ? "Nope" : null
+                                                return value === "cd816e04-a7cf-4df8-aad9-f021907cd81c" ? "error from local validation" : null
                                             }}
                                         />
+
+                                        <FKSelector
+                                            name="quxCId1"
+                                            label="async validation"
+                                            display={ "quxC1.name" }
+                                            query={ Q_QuxC }
+                                            searchFilter="name"
+                                            validateAsync={(ctx,value) => {
+                                                return value === "1098633c-9260-4884-a952-03c122efe53b" ? "error from async validation" : null
+                                            }}
+                                        />
+
 
                                         <ButtonToolbar>
                                             <Button className="btn btn-primary mr-1" transition="ok">
