3fbc7679f1f102dbf9f2bdc4bdab4e82e42d4132
Sven Helmberger <sven.helmberger@quinscape.de>
2023-03-16 19:01:56 +0100

    Query-Editor-Test: Add Test-Case loading
    Adds the new <TestCaseSelector/> with test data converted from the
    condition-editor-test extended to work for the QueryEditor
    
    Value example:
    
        {
            "rootType" : "AppUser",
            "fields" : [
                "roles",
                "login",
                "foos.created"
            ],
            "condition" : null,
            "sort" : [
                "login",
                "!foos.num"
            ]
        }
    
    rootType is the rootType for the query selector, fields the selected
    fields (with semi-intelligent defaults), condition the current condition
    of the query editor and sort the fields to be sorted by.

--- src/main/js/apps/shipping/processes/query-editor-test/query-editor-test.js
+++ src/main/js/apps/shipping/processes/query-editor-test/query-editor-test.js
@@ -1,11 +1,37 @@
-import {makeObservable, observable} from "mobx";
+import { action, makeObservable, observable } from "mobx"
 import QueryEditorTestHome from "./states/QueryEditorTestHome";
+import QueryTestCases from "../../../../data/condition/test-cases.json"
+
+import {config, QueryEditor, ViewState, createTreeRepresentationForInputSchema, decompileFilter} from "@quinscape/automaton-js";
 
 // noinspection JSUnusedGlobalSymbols
 export function initProcess(process, scope)
 {
     return QueryEditorTestHome;
 }
+
+
+function getDefaultFieldsForType(rootType)
+{
+    const inputSchema = config.inputSchema
+    const typeDef = inputSchema.getType(rootType)
+    if (!typeDef)
+    {
+        throw new Error("Invalid output type" + rootType);
+    }
+
+    const nameFields = inputSchema.getTypeMeta(rootType, "nameFields")
+    if (nameFields)
+    {
+        return nameFields;
+    }
+
+    if (typeDef.fields.some( fd => fd.name === "name"))
+        return ["name"]
+
+    return []
+}
+
 
 //TODO: create this state in query editor for condition editor, use with useMemo
 export default class QueryEditorTest {
@@ -14,64 +40,42 @@ export default class QueryEditorTest {
     rootType = "Foo"
 
     @observable
-    queryConfig = {
-        "select": [
-            "roles",
-            "login",
-            "foos.created"
-        ],
-        "where": {
-            "type": "Condition",
-            "name": "or",
-            "operands": [
-                {
-                    "type": "Condition",
-                    "name": "containsIgnoreCase",
-                    "operands": [
-                        {
-                            "type": "Field",
-                            "name": "foos.name"
-                        },
-                        // {
-                        //     "type": "Field",
-                        //     "name": "foos.num"
-                        // },
-                        {
-                            "type": "Value",
-                            "scalarType": "String",
-                            "value": "gfd"
-                        }
-                    ]
-                },
-                {
-                    "type": "Condition",
-                    "name": "equal",
-                    "operands": [
-                        {
-                            "type": "Field",
-                            "name": "login"
-                        },
-                        {
-                            "type": "Value",
-                            "scalarType": "String",
-                            "value": "gfdsgfdsxg"
-                        }
-                    ]
-                }
-            ]
-        },
-        "sort": [
-            "login",
-            "!foos.num"
-        ]
-    };
+    counter = 0
+
+    @observable
+    queryConfig = null;
 
 
     constructor()
     {
         makeObservable(this)
+
+        this.loadTestcase(QueryTestCases.AppUser)
     }
 
+
+    @action
+    loadTestcase(testCase)
+    {
+
+        let {
+            rootType,
+            fields : select =  getDefaultFieldsForType(testCase.rootType),
+            condition : where,
+            sort = getDefaultFieldsForType(testCase.rootType)
+        } = testCase
+
+        this.rootType = rootType;
+
+
+        this.queryConfig = {
+            select,
+            where,
+            sort
+        }
+
+        this.counter++;
+    }
 
 
 }
--- src/main/js/apps/shipping/processes/query-editor-test/states/QueryEditorTestHome.js
+++ src/main/js/apps/shipping/processes/query-editor-test/states/QueryEditorTestHome.js
@@ -1,9 +1,12 @@
 import React, {useState} from "react";
 import {config, QueryEditor, ViewState, createTreeRepresentationForInputSchema, decompileFilter} from "@quinscape/automaton-js";
-import {Col, Row} from "reactstrap";
+import { toJSON } from "@quinscape/automaton-js/filter";
+import { ButtonToolbar, Col, Row } from "reactstrap"
 import {Icon} from "domainql-form";
 import { toJS } from "mobx"
+import TestCaseSelector from "../../../../../components/TestCaseSelector"
 
+import QueryTestCases from "../../../../../data/condition/test-cases.json"
 
 const QueryEditorTestHome = new ViewState(
     "QueryEditorTestHome",
@@ -21,63 +24,80 @@ const QueryEditorTestHome = new ViewState(
 
     const { scope } = env;
 
-    console.log("unfiltered tree representation:", createTreeRepresentationForInputSchema("AppUser"));
+    //console.log("unfiltered tree representation:", createTreeRepresentationForInputSchema("AppUser"));
 
     const treeRepresentation = createTreeRepresentationForInputSchema("AppUser", ({fieldPath}) => {
         return !fieldPath.endsWith("bazLinks") && !fieldPath.endsWith("id");
     });
-    console.log("filtered tree representation:", treeRepresentation);
+    //console.log("filtered tree representation:", treeRepresentation);
 
     return (
-        <Row className="mt-3">
-            <Col>
-                <h1>Query Editor Test</h1>
-                <hr/>
-                <QueryEditor
-                    key={ scope.counter }
-                    className="mb-3"
-                    header={"Query Editor Test"}
-                    availableColumnTreeObject={treeRepresentation}
-                    rootType="AppUser"
-                    columnNameRenderer={(value, {origin, isDirectory}) => {
-                        if(origin === QueryEditor.ORIGIN_FIELD_SELECTION_TOKEN_LIST) {
-                            console.log(value, isDirectory);
-                        }
-                        if (isDirectory) {
-                            return (
-                                <>
-                                    <Icon className="fa-book mr-2"/>
-                                    {
-                                        value
-                                    }
-                                </>
-                            )
-                        } else {
-                            return value;
-                        }
-                    }}
-                    saveButtonOnClick={(queryConfiguration) => {
+        <>
+            <Row className="mt-3">
+                <Col>
+                    <h1>Query Editor Test</h1>
+                    <hr/>
+                    <QueryEditor
+                        key={ scope.counter }
+                        className="mb-3"
+                        header={"Query Editor Test"}
+                        availableColumnTreeObject={treeRepresentation}
+                        rootType={ scope.rootType }
+                        columnNameRenderer={(value, {origin, isDirectory}) => {
+                            if(origin === QueryEditor.ORIGIN_FIELD_SELECTION_TOKEN_LIST) {
+                                console.log(value, isDirectory);
+                            }
+                            if (isDirectory) {
+                                return (
+                                    <>
+                                        <Icon className="fa-book mr-2"/>
+                                        {
+                                            value
+                                        }
+                                    </>
+                                )
+                            } else {
+                                return value;
+                            }
+                        }}
+                        saveButtonOnClick={(queryConfiguration) => {
 
-                        const { select, where, sort } = queryConfiguration
+                            const { select, where, sort } = queryConfiguration
 
-                        console.log("QUERY EDITOR SAVE");
-                        console.log("SELECT", toJS(select))
-                        console.log("WHERE", decompileFilter(where))
-                        console.log("SORT", toJS(sort))
-                    }}
-                    onChange={(queryConfiguration) => {
+                            console.log("QUERY EDITOR SAVE");
+                            console.log("SELECT", toJS(select))
+                            console.log("WHERE", decompileFilter(where))
+                            console.log("SORT", toJS(sort))
+                        }}
+                        onChange={(queryConfiguration) => {
 
-                        const { select, where, sort } = queryConfiguration
+                            const { select, where, sort } = queryConfiguration
 
-                        console.log("QUERY EDITOR CHANGE");
-                        console.log("SELECT", toJS(select))
-                        console.log("WHERE", decompileFilter(where))
-                        console.log("SORT", toJS(sort))
-                    }}
-                    queryConfiguration={ scope.queryConfig }
-                />
-            </Col>
-        </Row>
+                            console.log("QUERY EDITOR CHANGE");
+                            console.log("SELECT", toJS(select))
+                            console.log("WHERE", decompileFilter(where))
+                            console.log("SORT", toJS(sort))
+                        }}
+                        queryConfiguration={ scope.queryConfig }
+                    />
+                    <hr/>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={6} >
+                    <ButtonToolbar>
+                        <div
+                        className="form form-inline">
+                            <TestCaseSelector
+                                label="Load Query Test-Case"
+                                cases={ QueryTestCases }
+                                onCaseSelect={ (testCase, name) => scope.loadTestcase(testCase) }
+                            />
+                        </div>
+                    </ButtonToolbar>
+                </Col>
+            </Row>
+        </>
     );
 });
 
